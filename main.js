c9056479e02ca3ba39d5694e96fcc40832e74d48
feefles <eyeung@seas.upenn.edu>
2014-10-11 14:59:04 -0400

    first pass

---
+++ main.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('#about').click(function() {
+		$('#content').html($('#aboutDiv').html());
+	})
+	$('#education').click(function() {
+		$('#content').html($('#educationDiv').html());
+	})
+	$('#projects').click(function() {
+		$('#content').html($('#projectDiv').html());
+	})
+});
