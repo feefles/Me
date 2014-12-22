d60f7bb3d131f254b2e8caa4c5b8881f7e1b8276
feefles <eyeung@seas.upenn.edu>
2014-12-22 01:53:03 -0500

    adding scss instead of sass and changed navbar

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 $(function() {
-	$('#content').html($('#aboutDiv').html());
+    $('#content').html($('#aboutDiv').html());
 
-	$('#about').click(function() {
-		$('#content').html($('#aboutDiv').html());
-	})
-	$('#education').click(function() {
-		$('#content').html($('#educationDiv').html());
-	})
-	$('#projects').click(function() {
-		$('#content').html($('#projectDiv').html());
-	})
+    $('#about').click(function() {
+        $('#content').fadeOut(0).html($('#aboutDiv').html()).fadeIn(200);
+    })
+    $('#education').click(function() {
+        $('#content').html($('#educationDiv').html());
+    })
+    $('#projects').click(function() {
+        $('#content').html($('#projectDiv').html());
+    })
+
+    $('#navigation i').click(function() {
+    	$('#navigation i').removeClass('selected');
+        $('#navigation i').find('span').css('display', 'none');
+
+
+    	$(this).addClass('selected');
+    	$(this).find('span').css('display', 'inline-block');
+
+    })
+
+
+    $('#navigation i').hover(function() {
+        $(this).find('span').css('display', 'inline-block');
+    }, function() {
+    	if ($(this).hasClass('selected')) return;
+        $(this).find('span').css('display', 'none');
+
+    })
 });
